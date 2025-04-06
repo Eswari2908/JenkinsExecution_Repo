eed8491cf328bc265a8d9c2cc929bdb990e38fed
Eswari <Eswari@LAPTOP-VC67H06G>
2025-04-06 11:27:42 +0530

    1st commit

---
+++ JenkinsExecution/src/test/java/Demo/JenkinsTest.java
@@ -0,0 +1,18 @@
+package Demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class JenkinsTest {
+	
+	@Test
+	
+	public void LaunchBrowser() {
+		
+		WebDriver driver = new ChromeDriver();
+		Reporter.log("Empty browser launched", true);
+	}
+
+}
